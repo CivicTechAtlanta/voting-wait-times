c2f6bb880e9dfeb976df698e266cc7c2e45294ff
Charles Julian Knight <charles@rabidaudio.com>
2016-04-27 01:52:01 -0400

    basic admin page with sorting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,8 @@ window.app = (function($, Firebase){
     this.db = new Firebase('https://voting-wait-times.firebaseio.com');
   };
 
-  App.prototype.addRoute = function(pattern, callback){
-    this.routes.push({ pattern: pattern, callback: callback });
+  App.prototype.addRoute = function(pattern, selector, callback){
+    this.routes.push({ pattern: pattern, selector: selector, callback: callback });
   };
 
   App.prototype.route = function(path){
@@ -18,8 +18,9 @@ window.app = (function($, Firebase){
       if(matches){
         var data = {};
         for(var j = 1; j < matches.length; j++){
-          data[keys[j-1]] = matches[j];
+          data[keys[j-1].substring(1)] = matches[j];
         }
+        $(route.selector).show();
         route.callback(data);
         return;
       }
@@ -40,112 +41,3 @@ window.app = (function($, Firebase){
   return app;
 
 })(window.window.jQuery, window.Firebase);
-
-
-
-  // setLocation($.QueryString['state'], $.QueryString['precinct']); //TODO
-
-  // if((!state || !precinct) && navigator.geolocation){
-  //   // try and get from geolocation
-  //   navigator.geolocation.getCurrentPosition(function(position){
-
-  //     console.log('got position', position);
-
-  //   });
-  // }
-
-
-//   drawTime();
-//   setInterval(drawTime, 15*1000);
-
-//   $('.btn-wait').click(function(){
-//     var wait = $(this).attr('wait');
-
-//     console.log('submit', wait);
-
-//     sendWait(wait);
-//   });
-// });
-
-
-
-
-// // { 'timestamp': '2016-03-05T00:00:00', 'precinct': '1', 'waittime': 2, 'reporterid': 'e3f99640d60577f72086b54087423593', 'state': 'GA'}
-// function addEstimate(precinctnum, state, email, waittime, callback){
-//   console.log('adding time');
-//   $.ajax({
-//     method: 'POST',
-//     url: '/waittimes',
-//     data: JSON.stringify({
-//       precinct: precinctnum,
-//       state: state,
-//       reporterid: md5(email),
-//       waittime: waittime,
-//       timestamp: new Date().toISOString()
-//     }),
-//     contentType: 'application/json',
-//     dataType: 'json',
-//     success: null
-//   }).then(callback);
-// }
-
-// function getTime(precinctnum, state, callback){
-//   console.log('getting time');
-//   $.ajax({
-//     method: 'GET',
-//     url: '/waittimes',
-//     data: {
-//       where: 'precinct = '+precinctnum+' AND state = \''+state+'\'',
-//       order: 'timestamp DESC',
-//       limit: 1
-//     },
-//     dataType: 'json'
-//   }).then(callback);
-// }
-
-
-// var waitTimeMap = [
-//   'No Line (whoo hoo!)',
-//   'Small Line (Less than 10 min)',
-//   'Medium Line (10-45 min)',
-//   'Long Line (Over 45 min)'
-// ];
-
-// function drawTime() {
-
-//     console.log('updating...');
-
-//     getTime(precinct, state, function(res){
-
-//       console.log('got data', res);
-
-//       $('.precinct-name').text(state + ' Precinct '+precinct);
-
-//       if(res[0]){
-//         var time = res[res.length-1];
-//         $('.wait-time').text(waitTimeMap[time.waittime]);
-
-//         $('.last-updated').text(new Date(time.timestamp).toRelativeString());
-//       }else{
-//         $('.wait-time').text('Unknown');
-
-//         $('.last-updated').text('Never');
-//       }
-//     });
-// }
-
-
-// function sendWait(time){
-//   var email = $('.email-field').val();
-
-//   addEstimate(precinct, state, email, time, function(){
-//     drawTime();
-//   });
-// }
-
-// function setLocation(state, precinct) {
-//   window.state = state;
-//   window.precinct = precinct;
-//   $('.state-field').val(state);
-//   $('.precinct-field').val(precinct);
-// }
